8d14c5564efab631504d0b08a1cad712b545dbc7
Shrawan Kumar <shrkumar@innominds.com>
2023-12-30 18:47:26 +0530

    fixed error again 8

--- utilities/readProperties.py
+++ utilities/readProperties.py
@@ -3,16 +3,10 @@ import os
 from pathlib import Path
 from configparser import ConfigParser
 
-<<<<<<< HEAD
-config_path= os.environ.get('CONFIG_PATH', 'Configurations/config.ini')
-#config = configparser.RawConfigParser()
-config= ConfigParser()
+config_path = os.environ.get('CONFIG_PATH', 'Configurations/config.ini')
+# config = configparser.RawConfigParser()
+config = ConfigParser()
 config.read('Configurations/config.ini')
-=======
-
-config = configparser.RawConfigParser()
-config.read('google_finance/Configurations/config.ini')
->>>>>>> c4a4a623eb2299b9694118ac1a8c572ea7d66af7
 
 
 class ReadConfig:
@@ -23,5 +17,5 @@ class ReadConfig:
 
     @staticmethod
     def variable():
-        var = config.get('variables','input')
+        var = config.get('variables', 'input')
         return var

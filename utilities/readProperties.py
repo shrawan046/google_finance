82140d130b1a962f17366da47ac0fb3a1aff559e
Shrawan Kumar <shrkumar@innominds.com>
2023-12-30 19:13:19 +0530

    fixed error again 12

--- utilities/readProperties.py
+++ utilities/readProperties.py
@@ -4,8 +4,8 @@ from pathlib import Path
 from configparser import ConfigParser
 
 
-print("Current directory:", os.getcwd())
-config_path = os.environ.get('CONFIG_PATH', 'google_finance/Configurations/config.ini')
+print("Read Property Current directory:", os.getcwd())
+config_path = os.environ.get('CONFIG_PATH', 'Configurations/config.ini')
 print("Config path:", config_path)
 # config = configparser.RawConfigParser()
 #config.read('Configurations/config.ini')
